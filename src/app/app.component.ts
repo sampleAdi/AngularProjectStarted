0b74eb7317bc65c4fd480fcd98a436c453814118
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-03-23 20:48:02 +0530

    site open on clicking logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']  // Optional if you want styles
 })
 export class AppComponent {
-  message = 'Hello from BridgeLabz';
-  logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';  // Online URL
+  title = 'Hello from BridgeLabz';
+  logoUrl: string = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';
+
+  openBridgeLabz() {
+    window.open('https://www.bridgelabz.com', '_blank');
+  }
 }
