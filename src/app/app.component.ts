c30672e10f98666dfee92ec8b31f9c226291a201
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-03-23 20:41:18 +0530

    Logo added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  // Optional if you want styles
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   message = 'Hello from BridgeLabz';
+  logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';  // Online URL
 }
