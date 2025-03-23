3cc9608e25b812116275226ed19a73b2b809a76e
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-03-23 22:09:59 +0530

    UserName input with BridgeLabz added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  // Optional if you want styles
+  styleUrls: ['./app.component.css'],
+  imports: [FormsModule, NgIf]
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
+  userName: string = '';
   logoUrl: string = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';
 
   openBridgeLabz() {
